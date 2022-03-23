d0442cf7885f015e7e2e71c2181fa838842d9381
EndRiKeR <solomenckov.petya@ya.ru>
2022-03-23 19:18:26 +0300

    remove b from folder

--- builds/lab3_2Dgraf/debug/moc_mainwindow.cpp
+++ builds/lab3_2Dgraf/debug/moc_mainwindow.cpp
@@ -7,7 +7,7 @@
 *****************************************************************************/
 
 #include <memory>
-#include "../../lab3_2Dgraf/mainwindow.h"
+#include "../../../lab3_2Dgraf/mainwindow.h"
 #include <QtCore/qbytearray.h>
 #include <QtCore/qmetatype.h>
 #if !defined(Q_MOC_OUTPUT_REVISION)
